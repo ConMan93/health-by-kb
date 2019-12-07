a0396c20c9d17c4b092ea8c3d45c9ee15c05b5b0
Connor Hoffman <connorhoffman@Connors-MacBook-Pro.local>
2019-12-07 14:34:21 -0700

    omg it worked

--- src/components/About.js
+++ src/components/About.js
@@ -5,10 +5,10 @@ import Image from 'react-bootstrap/Image';
 
 export default class About extends Component {
     render() {
-        // let image = 'https://scontent-den4-1.xx.fbcdn.net/v/t1.0-9/39972116_10214188789008432_3849217499618869248_n.jpg?_nc_cat=102&_nc_ohc=3cTsygL6oT8AQm2fXP1hQNBAl2K5NuzpLcd6CGlvF1uvCI1kQhW6XlBnA&_nc_ht=scontent-den4-1.xx&oh=25439580338b391e4a90b9f2e78a5038&oe=5E86BA01'
+        let image = 'https://scontent-den4-1.xx.fbcdn.net/v/t1.0-9/39972116_10214188789008432_3849217499618869248_n.jpg?_nc_cat=102&_nc_ohc=3cTsygL6oT8AQm2fXP1hQNBAl2K5NuzpLcd6CGlvF1uvCI1kQhW6XlBnA&_nc_ht=scontent-den4-1.xx&oh=25439580338b391e4a90b9f2e78a5038&oe=5E86BA01'
         return (
             <div className='about-me' >
-                {/* <Image src={`${image}`} roundedCircle width='250px' height='250px' /> */}
+                <Image src={`${image}`} roundedCircle width='250px' height='250px' />
                 <p className='about-me-content'>
                 Hi I’m Kristi and I have a passion for health and helping people. I enjoy exercising, hanging out with friends, 
                 and crafting. Summer is my favorite season and I wish it could be 80 degrees year round☀️ <br></br><br></br>
